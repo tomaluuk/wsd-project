33c9f3ae2127f764bd9f48c2714111b914ad7877
luukkane <82313983+luukkane@users.noreply.github.com>
2022-06-25 19:00:34 +0300

    remove deprecated package bodyParser

--- src_server/server.js
+++ src_server/server.js
@@ -1,18 +1,17 @@
 //requiring neessary npm middleware packages
-var express = require('express');
-var bodyParser = require("body-parser");
+var express = require("express");
 var session = require("express-session");
 const Sequelize = require("sequelize");
-var passport = require("passport")
+var passport = require("passport");
 
 //Setting up port
-var PORT  = process.env.PORT || 8800;
+var PORT = process.env.PORT || 8800;
 //Creating express app and configuring middleware
 // needed to read through our public folder
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(express.static("build"));
 
@@ -21,20 +20,20 @@ Items.sync()
     .then(() => console.log('Items table created successfully'))
     .catch(err => console.log('Error while syncing Items table: ' + err));
 */
-    
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', function (req, res) {
-    res.sendFile('index.html')
+app.get("/", function (req, res) {
+  res.sendFile("index.html");
 });
 
 // API routes
-var routes = require('./router/api')(app);
-app.use('/',routes);
+var routes = require("./router/api")(app);
+app.use("/", routes);
 
-// This will listen to and show all activities on our terminal to 
+// This will listen to and show all activities on our terminal to
 //let us know what is happening in our app
-app.listen(PORT, function() {
-    console.log("app listening on PORT " + PORT);
+app.listen(PORT, function () {
+  console.log("app listening on PORT " + PORT);
 });
