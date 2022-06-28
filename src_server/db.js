3bf8507e3c25d469935798ccf5467d07878f7ce5
luukkane <82313983+luukkane@users.noreply.github.com>
2022-06-28 16:33:03 +0300

    convert db.js into ES module

--- src_server/db.js
+++ src_server/db.js
@@ -1,23 +1,23 @@
-const Sequelize = require("sequelize");
+import Sequelize from "sequelize";
 
 //initialize an instance of Sequelize
 const sequelize = new Sequelize({
-    database: 'jussi',
-    username: 'root',
-    password: 'pirjo',
-    dialect: 'mysql'
+  database: "jussi",
+  username: "root",
+  password: "pirjo",
+  dialect: "mysql",
 });
 
 //check the database connection
 sequelize
-    .authenticate()
-    .then(() => console.log('Connection has been established.'))
-    .catch(err => console.error('Unable to connect to the database: ', err));
-   
-var UserModel = require('./models/users');
-var ListModel = require('./models/lists');
-var SubscriptionModel = require('./models/subscriptions');
-var ItemModel = require('./models/items');
+  .authenticate()
+  .then(() => console.log("Connection has been established."))
+  .catch((err) => console.error("Unable to connect to the database: ", err));
+
+import UserModel from "./models/users.js";
+import ListModel from "./models/lists.js";
+import SubscriptionModel from "./models/subscriptions.js";
+import ItemModel from "./models/items.js";
 
 const Users = UserModel(sequelize, Sequelize);
 const Items = ItemModel(sequelize, Sequelize);
@@ -25,14 +25,15 @@ const Subscriptions = SubscriptionModel(sequelize, Sequelize);
 const Lists = ListModel(sequelize, Sequelize);
 
 // Create database and tables if doesn't exist
-sequelize.sync()//{force:true}
-.then(()=>{
-  console.log('Database and tables created!!');
-})
+sequelize
+  .sync() //{force:true}
+  .then(() => {
+    console.log("Database and tables created!!");
+  });
 
-module.exports = {
-    Users,
-    Lists,
-    Items,
-    Subscriptions
-}
+export default {
+  Users,
+  Lists,
+  Items,
+  Subscriptions,
+};
